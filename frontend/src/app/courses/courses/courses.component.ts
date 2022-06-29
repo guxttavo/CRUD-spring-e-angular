8ca1ba42f4ff237e16b7ebf3d90056d2b795f7f1
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-28 21:40:21 -0300

    http call

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
@@ -10,11 +11,11 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private courseService: CoursesService) {
-
+    this.courses = this.courseService.list();
   }
 
   ngOnInit(): void {
--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap, first, delay } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -8,11 +9,15 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json'
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'frontend' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+        first(),
+        tap(courses => console.log(courses))
+      );
   }
 }
