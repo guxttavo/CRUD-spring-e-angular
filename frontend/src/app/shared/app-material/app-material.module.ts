8d5845b98c192d90b1dceb779889e396f6f4b120
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-28 22:25:52 -0300

    pipe to show icons

--- frontend/src/app/shared/app-material/app-material.module.ts
+++ frontend/src/app/shared/app-material/app-material.module.ts
@@ -4,13 +4,16 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
   ]
 
 })
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { AppMaterialModule } from './app-material/app-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
+    CategoryPipe,
   ],
   imports: [
     CommonModule,
     AppMaterialModule
   ],
   exports: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ]
 })
 export class SharedModule { }
